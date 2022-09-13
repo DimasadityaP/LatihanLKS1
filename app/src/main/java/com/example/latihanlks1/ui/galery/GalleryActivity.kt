5d0597a74cbd98a4bd0dbd35f0035a8b6baa0918
Naufal <m.naufal.fadhillah98@gmail.com>
2022-09-13 22:13:48 +0700

    add log

--- app/src/main/java/com/example/latihanlks1/ui/galery/GalleryActivity.kt
+++ app/src/main/java/com/example/latihanlks1/ui/galery/GalleryActivity.kt
@@ -2,12 +2,13 @@ package com.example.latihanlks1.ui.galery
 
 import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.latihanlks1.data.model.Album
 import com.example.latihanlks1.data.model.Photo
 import com.example.latihanlks1.data.network.NetworkApi
@@ -23,8 +24,8 @@ import org.json.JSONObject
 import java.io.File
 
 class GalleryActivity : AppCompatActivity() {
-    lateinit var binding : ActivityGalleryBinding
-    lateinit var galleryadapter : GalleryAdapter
+    lateinit var binding: ActivityGalleryBinding
+    lateinit var galleryadapter: GalleryAdapter
     private var list = listOf<Photo>()
     private var searchlist = listOf<Photo>()
 
@@ -37,14 +38,16 @@ class GalleryActivity : AppCompatActivity() {
     }
 
     private val galleryLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if (it.resultCode == RESULT_OK){
-                it.data?.data?.let { uri->
-                    getRealPathFromURI(uri)?.let {
-                        val file = File(it)
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                Log.d("ActivityResultGallery", it.data?.data.toString())
+                it.data?.data?.let { uri ->
+                    getRealPathFromURI(uri)?.let { realPath ->
+                        Log.d("getRealPathFromURI", realPath)
+                        val file = File(realPath)
                         CoroutineScope(Dispatchers.Main).launch {
                             postImage(file)?.let {
-                                Log.d("postImage", it.toString())
+                                Log.d("postImage", realPath)
                             }
                         }
                     }
@@ -53,11 +56,11 @@ class GalleryActivity : AppCompatActivity() {
         }
 
     private val storagePermission =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
-            permissions ->
-            when{
-                permissions[Manifest.permission.READ_EXTERNAL_STORAGE] == true ->{
-                    val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                permissions[Manifest.permission.READ_EXTERNAL_STORAGE] == true -> {
+                    val intent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                     intent.putExtra(
                         Intent.EXTRA_MIME_TYPES,
                         arrayOf("image/jpg", "image/jpeg", "image/png")
@@ -69,7 +72,7 @@ class GalleryActivity : AppCompatActivity() {
             }
         }
 
-    private fun setUp(){
+    private fun setUp() {
         galleryadapter = GalleryAdapter { photo ->
             val intent = Intent(this, DetailActivity::class.java)
             CoroutineScope(Dispatchers.Main).launch {
@@ -79,21 +82,23 @@ class GalleryActivity : AppCompatActivity() {
                     val nameCat = it.getString("name")
                     val desc = it.getString("description")
                     val imgurl = photo.image.toString()
-                    val album = Album(id = idCat, name = nameCat , descrption = desc, imageUrl = imgurl)
+                    val album =
+                        Album(id = idCat, name = nameCat, descrption = desc, imageUrl = imgurl)
                     intent.putExtra(DetailActivity.EXTRA_ALBUM, album)
                     startActivity(intent)
-                    }
                 }
             }
+        }
         binding.RvImage.adapter = galleryadapter
         connectionCatApi()
 
-        binding.btnAdd.setOnClickListener{
+        binding.btnAdd.setOnClickListener {
             storagePermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
         }
     }
+
     private suspend fun getCats(): JSONArray? {
-        return withContext(Dispatchers.IO){
+        return withContext(Dispatchers.IO) {
             NetworkApi("https://api.thecatapi.com/v1/breeds?limit=20&page=0")
                 .execute()?.let {
                     JSONArray(it)
@@ -101,31 +106,42 @@ class GalleryActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun postImage(file: File) : JSONObject?{
-        return withContext(Dispatchers.IO){
-            NetworkApi("https://api.thecatapi.com/v1/images/upload", method = "POST")
-                .addFilePart("file", file)
-                .execute()?.let {
-                    JSONObject(it)
-                }
+    private suspend fun postImage(file: File): JSONObject? {
+        return try {
+            withContext(Dispatchers.IO) {
+                NetworkApi(
+                    "https://api.thecatapi.com/v1/images/upload",
+                    method = "POST",
+                    headers = arrayOf(
+                        Pair("x-api-key", "live_F9QVdZ5GmaliK9O0lugJmvTckMYhL74IrOll5JGyPA2UchBKC5CvPCD7s0lUBb7d"),
+                    ),
+                ).addFilePart("file", file)
+                    .execute()?.let {
+                        JSONObject(it)
+                    }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this@GalleryActivity, e.localizedMessage, Toast.LENGTH_SHORT).show()
+            null
         }
     }
 
-    private suspend fun getDetailCats(id : String?) : JSONObject?{
-        return withContext(Dispatchers.IO){
+    private suspend fun getDetailCats(id: String?): JSONObject? {
+        return withContext(Dispatchers.IO) {
             NetworkApi("https://api.thecatapi.com/v1/breeds/$id")
                 .execute()?.let {
                     JSONObject(it)
                 }
         }
     }
-    private fun connectionCatApi(){
+
+    private fun connectionCatApi() {
         CoroutineScope(Dispatchers.Main).launch {
             val jsonArray = getCats()
             jsonArray?.let {
                 val templist = mutableListOf<Photo>()
 
-                for (i in 0 until it.length()){
+                for (i in 0 until it.length()) {
                     val item = it.getJSONObject(i)
                     val image = item.getJSONObject("image")
                     templist.add(
